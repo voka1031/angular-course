4d2fbcddb8f223e3b09da586c83114c3000752c9
voka1031 <voka1031@gmail.com>
2018-12-06 17:50:34 +0800

    update recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('recipe 1', 'A test recipe', 'https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg'),
+    new Recipe('recipe 2', 'A test recipe', 'https://www.seriouseats.com/recipes/images/2017/03/20170210-vietnamese-chicken-emily-matt-clifton-3.jpg'),
+  ];
 
   constructor() { }
 
