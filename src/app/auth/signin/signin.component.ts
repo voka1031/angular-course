edaa870d6ee1e77ca08dcb714d5113ef962dbdbb
voka1031 <voka1031@gmail.com>
2018-12-20 17:59:08 +0800

    clean code

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './../auth.service';
 
@@ -7,23 +7,12 @@ import { AuthService } from './../auth.service';
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent {
 
   constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
-
   onSignin(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.authService.signinUser(email, password);
+    this.authService.signinUser(form.value.email, form.value.password);
   }
 
-  onDefaultData(form: NgForm) {
-    form.reset({
-      email: 'test@test.com',
-      password: '123456'
-    })
-  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,22 +1,17 @@
-import { AuthService } from './../auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
   constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-  }
-
   onSignup(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.authService.signupUser(email, password);
+    this.authService.signupUser(form.value.email, form.value.password);
   }
 }
