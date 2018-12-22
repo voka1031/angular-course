b330299b96764b5313e61846e84816cf80b26e5c
voka1031 <voka1031@gmail.com>
2018-12-22 17:47:09 +0800

    add shorten pipe

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './../shared/shared.module';
+import { ShortenPipe } from './../shared/shorten.pipe';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
@@ -18,6 +19,7 @@ import { RecipesComponent } from './recipes.component';
         RecipeItemComponent,
         RecipeEditComponent,
         RecipeDetailComponent,
+        ShortenPipe
     ],
     imports: [
         CommonModule,
---
+++ src/app/shared/shorten.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+
+    transform(value: any) {
+        if (value.length > 50) {
+            return value.substr(0, 50) + "...";
+        }
+        return value;
+    }
+
+}
